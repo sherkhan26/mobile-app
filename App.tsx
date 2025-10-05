c9345ed9e165c9aef19f3ce7c4c53f1e39d48511
Шерхан Амангелды <sherhan_26@mail.ru>
2025-10-05 23:51:06 +0500

    add Header component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View } from 'react-native'
+
+import Header from '@/components/Header'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Header />
+			<StatusBar style='light' />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#333',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
+})
